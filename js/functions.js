966387ed88d7c019966840d39815d7d7356d6dec
pablo bracciale <pabloibracciale@gmail.com>
2022-07-26 18:39:07 -0300

    creacion de secciones de forma dinamica

--- js/functions.js
+++ js/functions.js
@@ -1,37 +1,44 @@
 let usuario=document.getElementsByClassName("inputUsuario");
-let contraseña=document.getElementById("inputContraseña");
+let contraseña=document.getElementsByClassName("inputContraseña");
 let ingresar=document.getElementsByClassName("enlaceIngresar")[0];
-let zonaIngresar=document.getElementsByClassName("zonaIngresar");
-let usuarioValor;
-let contraseñaValor;
-let recomendados=document.getElementsByClassName("recomendados")
+let masEscuchados=document.getElementsByClassName("tusEscuchados");
+let recomendados=document.getElementsByClassName("recomendados");
+let tendencia=document.getElementsByClassName("tendencia");
 
-/*
-artistasRecomendados.forEach(e => {
-    let card=document.createElement("div");
-    recomendados[0].appendChild(card);
-    let nombre=document.createElement("p");
-    let genero=document.createElement("p");
-    card.appendChild(nombre);
-    card.appendChild(genero);
-    card.classList.add("card");
-    nombre.innerText=e.nombre;
-    genero.innerText=e.genero;
-    nombre.classList.add("card-title");
-    genero.classList.add("card-genre");
-});
-*/
+//pensar como incorporar imagenes dinamicamentes//
+
+function crearSeccion (seccion, artistas) {
+    artistas.forEach(e => {
+        let card=document.createElement("div");
+        seccion[0].appendChild(card);
+        let nombre=document.createElement("p");
+        let genero=document.createElement("p");
+        card.appendChild(nombre);
+        card.appendChild(genero);
+        card.classList.add("card");
+        nombre.innerText=e.nombre;
+        genero.innerText=e.genero;
+        nombre.classList.add("card-title");
+        genero.classList.add("card-genre");
+    })
+}
+
+// mejorar con un ciclo //
+crearSeccion (masEscuchados, artistasEscuchados);
+crearSeccion (recomendados, artistasRecomendados);
+crearSeccion (tendencia, artistasTendencia);
+
 ingresar.addEventListener("click", function(){
     console.log(usuario[0].value)
+    console.log(contraseña[0].value)
     allClients.forEach(e => {
-        if (usuario[0].value==e.user) {
+        if (usuario[0].value==e.user&&contraseña[0].value==e.password) {
+            //habilita ingreso//
             console.log("Ingresa")
         } else {
+            //no habilita ingreso//
             console.log("no ingresa")
         }
     });
     }
 )
-/*
-&&contraseña==e.password
-*/
--- js/music.js
+++ js/music.js
@@ -13,8 +13,22 @@ let ray = new Artista (2, "Ray Charles", "R&B");
 let miles = new Artista (3, "Miles Davis", "Jazz");
 let beatles = new Artista (4, "The Beatles", "Rock");
 let rolling = new Artista (5, "The Rolling Stones", "Rock");
+let beastie=new Artista (6, "Beastie Boys", "Rap");
+let jamesBrown= new Artista (7,"James Brown", "Soul");
+let oasis=new Artista (8, "Oasis", "Rock");
+let frank=new Artista (9, "Frank Sinatra", "Swing");
+let police=new Artista (10, "The Police", "Rock");
+let soda=new Artista (11, "Soda Stereo", "Rock");
+let daft=new Artista (12, "Daft Punk", "Electro")
+let babasonicos=new Artista (13, "Babasonicos", "Rock");
+let pericos=new Artista (14, "Los Pericos", "Reggae");
+let bob=new Artista (15, "Bob Marley", "Reggae");
 
-let artistasRecomendados =[]
+let artistasEscuchados = [];
+let artistasRecomendados=[]; 
+let artistasTendencia=[];
 
+artistasEscuchados.push (beastie,jamesBrown,oasis,frank,police);
 artistasRecomendados.push(jimi, ray, miles, beatles, rolling);
+artistasTendencia.push(soda, daft,babasonicos,pericos,bob);
 
